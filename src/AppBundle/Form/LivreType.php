2cd1ff2543e916a6387dbb3af59f992c9b120a57
christopher-fourgeaud <christopher.fourgeaud@gmail.com>
2018-11-30 00:49:34 +0100

    Création du formulaire pour les livres

---
+++ src/AppBundle/Form/LivreType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('dateParution')
+            ->add('dateAjout')
+            ->add('photo')
+            ->add('categorie')
+            ->add('auteur');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Livre',
+            'csrf_protection' => false
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_livre';
+    }
+
+
+}
